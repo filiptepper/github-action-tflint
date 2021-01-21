afaf130a43c8728c49cab7d663681702b57953ec
Filip Tepper <filip@tepper.pl>
2021-01-21 09:24:23 +0100

    separate files

--- exec/wrapper-tflint.sh
+++ exec/wrapper-tflint.sh
@@ -5,6 +5,7 @@ _FILES=$(printf '%s' "$1" | jq -r '.[]')
 for _FILE in $_FILES
 do
   if [ "${_FILE: -3}" == ".tf" ]; then
-    tflint -f checkstyle "$_FILE" -c "$2" | reviewdog -f=checkstyle -reporter=github-pr-review
+    tflint -f checkstyle "$_FILE" -c "$2" > _TFLINT
+    cat _TFLINT | reviewdog -f=checkstyle -reporter=github-pr-review
   fi
 done
