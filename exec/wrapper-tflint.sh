a95b850a02aa563164d6a0defbc66f230a386dce
Filip Tepper <filip@tepper.pl>
2021-01-21 09:12:45 +0100

    handle external files list

--- exec/wrapper-tflint.sh
+++ exec/wrapper-tflint.sh
@@ -5,6 +5,6 @@ _FILES=$(printf '%s' "$1" | jq -r '.[]')
 for _FILE in $_FILES
 do
   if [ "${_FILE: -3}" == ".tf" ]; then
-    tflint -f checkstyle "$_FILE" -c "$1" | reviewdog -f=checkstyle -reporter=github-pr-review
+    tflint -f checkstyle "$_FILE" -c "$2" | reviewdog -f=checkstyle -reporter=github-pr-review
   fi
 done
