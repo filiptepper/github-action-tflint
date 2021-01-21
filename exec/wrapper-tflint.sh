e8cf26a2cec46cc7ea5bc87a1b78bcc3bbf3af2f
Filip Tepper <filip@tepper.pl>
2021-01-21 09:06:27 +0100

    initial commit

---
+++ exec/wrapper-tflint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash +e
+
+_FILES=$(printf '%s' "$1" | jq -r '.[]')
+
+for _FILE in $_FILES
+do
+  if [ "${_FILE: -3}" == ".tf" ]; then
+    tflint -f checkstyle "$_FILE" -c "$1" | reviewdog -f=checkstyle -reporter=github-pr-review
+  fi
+done
